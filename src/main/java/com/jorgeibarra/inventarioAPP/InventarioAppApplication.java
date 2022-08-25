ca00cac18a02e0a54b73575312a61e4b0c320a5c
unknown <jorgeibarra87@gmail.com>
2022-08-25 14:56:48 -0500

    Inicializacion del proyecto inventarioAPP

---
+++ src/main/java/com/jorgeibarra/inventarioAPP/InventarioAppApplication.java
@@ -0,0 +1,13 @@
+package com.jorgeibarra.inventarioAPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioAppApplication.class, args);
+	}
+
+}
